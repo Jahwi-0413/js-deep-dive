39afef4317a7ab1d7f94c0912518a335aba217e6
jaehe <jaehe0413@naver.com>
2023-05-30 10:51:29 +0900

    04장

---
+++ scripts/04-05-호이스팅.js
@@ -0,0 +1,23 @@
+//var test
+
+console.log(v); //undefined
+var v;
+v = 10;
+console.log(v); //10
+
+//const test
+/*
+console.log(c); //ReferenceError
+const c = 10;
+console.log(c);
+*/
+
+//let test
+/*
+console.log(l); //ReferenceError
+let l = 10;
+console.log(l);
+*/
+
+//모든 변수의 선언을 호이스팅해주지만
+//undefined로 초기화해주는건 var 변수만 지원된다
